f6e238a9b61b908d66da40d73492bfc82433faab
Pedro Bittencourt <pbittenc@gmail.com>
2021-10-27 17:10:58 -0300

    implementação de update e delete

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -24,12 +24,38 @@ class PessoaController {
         }
     }
 
-    static async criaPessoa(req, res){
+    static async criaPessoa(req, res) {
         const novaPessoa = req.body
-        try{
+        try {
             const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
             return res.status(200).json(novaPessoaCriada)
-        }catch(error){
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(pessoaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json({mensagem: `id ${id} deletado`})
+        } catch (error) {
             return res.status(500).json(error.message)
         }
     }
